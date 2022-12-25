431403f0db1ee30770bf6d5b57df3b10992931c9
giobruno <giobruno@me.com>
2022-12-25 17:51:37 +0100

    set up theme mode function

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,23 @@
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import { useState } from "react";
+
+const Sidebar = () => {
+  //const theme = useTheme();
+  //const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  return <div></div>;
+};
+export default Sidebar;
---
+++ src/scenes/global/topbar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function topbar() {
+  return <div>topbar</div>;
+}
